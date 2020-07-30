c3af0a54c9174b50c63913ab1a97528d2d2da6dc
KaylarW <741625687@qq.com>
2020-07-30 17:11:00 +0800

    '完成图片懒加载'

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from './router'
 import store from './store'
 
 import FastClick from 'fastclick'
+import VueLazyLoad from 'vue-lazyload'
 
 import toast from 'components/common/toast'
 
@@ -14,8 +15,13 @@ Vue.prototype.$bus = new Vue()
 //安装 toast 插件
 Vue.use(toast)
 
-//解决 300ms 延迟
+//使用 fastclick 插件，解决点击 300ms 延迟
 FastClick.attach(document.body)
+
+//使用 lazyload 图片懒加载插件
+Vue.use(VueLazyLoad,{
+  loading: require('./assets/img/common/waiting.jpg')
+})
 
 new Vue({
   render: h => h(App),
