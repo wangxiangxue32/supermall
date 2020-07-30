afbbaf88cec1d72c8f63090640fc097a13fc4584
KaylarW <741625687@qq.com>
2020-07-30 18:35:37 +0800

    '完成分类页面'

--- src/views/category/Category.vue
+++ src/views/category/Category.vue
@@ -1,15 +1,154 @@
 <template>
-  <div>
-    <h2>分类</h2>
+  <div class="category">
+    <nav-bar class="nav-bar">
+      <div slot="center">商品分类</div>
+    </nav-bar>
+
+    <div class="content">
+      <tab-menu :categories="categories"
+                @selectItem="selectItem"/>
+      <scroll id="tab-content" :data="[categoryData]">
+        <div>
+          <tab-content-category :subcategories="showSubCategory"/>
+          <tab-control :title="['综合','新品','销量']"
+                       @itemClick="tabClick"/>
+          <tab-content-detail :category-detail="showCategoryDetail"/>
+        </div>
+      </scroll>
+    </div>
   </div>
 </template>
 
 <script>
+  import NavBar from "components/common/navbar/NavBar";
+  import Scroll from "components/common/scroll/Scroll";
+  import TabControl from "components/content/tabControl/TabControl";
+
+  import TabMenu from "./childComps/TabMenu";
+  import TabContentCategory from "./childComps/TabContentCategory";
+
+  import {getCategory,getSubCategory,getCategoryDetail} from "network/category";
+  import {POP,SELL,NEW} from "common/const";
+  import TabContentDetail from "./childComps/TabContentDetail";
+
   export default {
-    name: "category"
+    name: "category",
+    components: {
+      TabContentDetail,
+      Scroll,
+      NavBar,
+      TabControl,
+
+      TabMenu,
+      TabContentCategory
+    },
+    data(){
+      return {
+        categories: [],
+        categoryData: {},
+        currentIndex: -1
+      }
+    },
+    created() {
+      //请求商品数据
+      this.getCategory()
+    },
+    computed: {
+      showSubCategory() {
+        if (this.currentIndex === -1) return {}
+        return this.categoryData[this.currentIndex].subcategories
+      },
+      showCategoryDetail() {
+        if (this.currentIndex === -1) return []
+        return this.categoryData[this.currentIndex].categoryDetail[this.currentType]
+      }
+    },
+    methods:{
+      tabClick(index){
+        switch (index) {
+          case 0:
+            this.currentType = 'pop'
+            break
+          case 1:
+            this.currentType = 'new'
+            break
+          case 2:
+            this.currentType = 'sell'
+            break
+        }
+        this.$refs.tabControl1.currentIndex = index
+        this.$refs.tabControl2.currentIndex = index
+      },
+      selectItem(index){
+        this._getSubCategories(index)
+      },
+      getCategory() {
+        getCategory().then(res => {
+          // 1.获取分类数据
+          this.categories = res.data.category.list
+          // 2.初始化每个类别的子数据
+          for (let i = 0; i < this.categories.length; i++) {
+            this.categoryData[i] = {
+              subcategories: {},
+              categoryDetail: {
+                'pop': [],
+                'new': [],
+                'sell': []
+              }
+            }
+          }
+          // 3.请求第一个分类的数据
+          this._getSubCategories(0)
+        })
+      },
+      _getSubCategories(index){
+        this.currentIndex = index
+        const mailKey = this.categories[index].maitKey
+        getSubCategory(mailKey).then(res => {
+          this.categoryData[index].subcategories = res.data
+          this.categoryData = {...this.categoryData}
+          this._getCategoryDetail(POP)
+          this._getCategoryDetail(SELL)
+          this._getCategoryDetail(NEW)
+        })
+      },
+      _getCategoryDetail(type){
+        //1.获取请求的 miniWallkey
+        const miniWallkey = this.categories[this.currentIndex].miniWallkey
+
+        //2.发送请求，传入 miniWallkey 和 type
+        getCategoryDetail(miniWallkey,type).then(res => {
+          //3.将获取的数据保存下来
+          this.categoryData[this.currentIndex].categoryDetail[type] = res
+          this.categoryData = {...this.categoryData}
+        })
+      }
+    }
   }
 </script>
 
 <style scoped>
+  .category {
+    height: 100vh;
+  }
 
+  .nav-bar{
+    color: white;
+    background-color: #ff8198;
+  }
+
+  .content {
+    position: absolute;
+    left: 0;
+    right: 0;
+    top: 44px;
+    bottom: 49px;
+    display: flex;
+  }
+
+  #tab-content {
+    height: 100%;
+    flex: 1;
+    overflow: hidden;
+  }
 </style>
--- src/views/mine/Mine.vue
+++ src/views/mine/Mine.vue
@@ -1,15 +1,24 @@
 <template>
   <div>
-    <h2>我的</h2>
+    <nav-bar class="nav-bar">
+      <div slot="center">个人中心</div>
+    </nav-bar>
   </div>
 </template>
 
 <script>
+  import NavBar from "components/common/navbar/NavBar";
   export default {
-    name: "Mine"
+    name: "Mine",
+    components: {
+      NavBar
+    }
   }
 </script>
 
 <style scoped>
-
+  .nav-bar{
+    color: #fff;
+    background-color: #ff8198;
+  }
 </style>
